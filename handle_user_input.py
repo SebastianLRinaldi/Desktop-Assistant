15e84a7e64b8f92743657c3df53bc56b4e2293a2
Sebastian <epicsauce2013@gmail.com>
2024-02-11 12:54:13 -0800

    Added comments wiht the get_input() fuction to better undertand what I wrote and what needs changed

--- handle_user_input.py
+++ handle_user_input.py
@@ -12,11 +12,18 @@ def execute_user_command():
     # Get the user's choice
     exit_command_received = False
     while not exit_command_received: 
+
+        # Prompt user to enter a command
         command = input("Enter a command (or 'exit' to quit): ")
+
+        # logic for exit 
         if command.lower() == 'exit':
             exit_command_received = True
-
+        
+        # commare user input to names of commands 
+        
         elif command in functions:
+            #   if there is a match get functions number of args for that function
             selected_function = functions[command]['function']
             num_params = len(inspect.signature(selected_function).parameters) # get number of parameters dynamically
             print(num_params)
@@ -25,6 +32,11 @@ def execute_user_command():
             print(user_parameter)
             print(*user_parameter)
 
+            # with function found by user command 
+            # and 
+            # with args passed in by the user 
+            # run the function with those args
+            # and show result 
             result = selected_function(*user_parameter)
             print(f"Command Executed: Returns - {result}\n")
 
@@ -37,6 +49,8 @@ def get_input(num_params, command):
     for parameter_index in range(num_params):
         description = functions[command]['description']
         needed_param = functions[command]['needed parameters'][parameter_index]
+        # tell user what args they need to give and which ones are optional
+        # don't have a built in yet for prompts specifc to commands
         prompt = f"{description}\nEnter {needed_param}:"
         user_input = input(prompt)
         inputs.append(user_input)
